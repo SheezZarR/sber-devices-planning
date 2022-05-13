b2f181773cebb4d8c17fec4d0cfcd48d7c0be262
Starman-in-the-sky <ms.practice@yandex.ru>
2022-05-13 20:55:38 +0300

    Task_list + Task_item_list

--- front/src/components/Task_list.jsx
+++ front/src/components/Task_list.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import {TaskItem} from "./Task_list_item";
+import '../Styles/Task_list_item.css'
 
 const Task_list = (props) => {
+    const[task_items, set_task_items] = useState([
+        {id: 1, text: 'Manacrypt'},
+        {id: 2, text: 'Manacrypt'},
+        {id: 3, text: 'Manacrypt'},
+        {id: 4, text: 'Manacrypt'},
+    ])
     return (
         <div className="Task_list">
-            <TaskItem/>
-            <TaskItem/>
-            <TaskItem/>
-            <TaskItem/>
+
+            {task_items.map(task_item =>
+                <TaskItem task_item={task_item} key={task_item.id}/>
+            )}
         </div>
     );
 }
+
 export default Task_list;
--- front/src/components/Task_list_item.jsx
+++ front/src/components/Task_list_item.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import {Checkbox} from "@salutejs/plasma-ui";
 
-
 export const TaskItem = (props) => {
     return(
         <div className="task_item">
             <div className="task_item_content">
-                <Checkbox label="DMnmfnfnfuneffnenfenfunefeffeefef"  defaultChecked />
+                <Checkbox label={props.task_item.text} id={props.task_item.id} defaultChecked />
             </div>
         </div>
     )
