04a808b7c8ccfeeae140ba64809c401e20129942
Iaroslav 'SheezZarR' Baslin <wertout77@gmail.com>
2022-05-24 11:22:48 +0300

    Added Salute component

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+
+import React, { 
+	useEffect,
+	useRef 
+} from 'react'
 import {
-    createSmartappDebugger,
-    createAssistant,
+	AssistantAppState,
+	createSmartappDebugger,
+	createAssistant,
 } from "@sberdevices/assistant-client";
 
 import {BrowserRouter , Route, Routes, Switch} from "react-router-dom";
@@ -16,17 +21,39 @@ import './Styles/Task_list_item.css'
 import './Styles/Calendar.css'
 import './Styles/App.css'
 
+
+const initAssistant = (getState) => {
+	if (process.env.NODE_ENV === "development") {
+		return createSmartappDebugger({
+			token: process.env['REACT_APP_TOKEN'] ?? "",
+			initPhrase: "Открой Plan Up",
+			getState
+		});
+	}
+	return createAssistant({getState});
+}
+
 const App = () => {
+	const assistantStateRef = useRef(null);
+	const assistant = useRef(null);
+
+	useEffect(() => {
+		assistant.current = initAssistant(() => assistantStateRef.current);
+		assistant.current.on("data", ({ action }) => {
+			console.log(action);
+		}) 
+	});
+  
   return (
-        <div className='app-wrapper'>
-            <Background_app/>
-            <Navbar/>
-            <div class='app-wrapper-content'>
-                <Tasks/>
-                {/*<Achievements/>
-                <Profile/>*/}
-            </div>
-        </div>
+		<div className='app-wrapper'>
+			<Background_app/>
+			<Navbar/>
+			<div class='app-wrapper-content'>
+				<Tasks/>
+				{/*<Achievements/>
+				<Profile/>*/}
+			</div>
+		</div>
   );
 }
 
